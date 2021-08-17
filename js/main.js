49631b150a0fd224e5eb1bd823b12c0fc23b38db
felipetrevisanutto <felipe.midiacom@gmail.com>
2021-08-16 21:14:17 -0300

    primeira parte da aula

---
+++ js/main.js
@@ -0,0 +1,61 @@
+
+
+var d = new Date();
+alert(d);
+alert(d.getMonth()+1);
+alert(d.getHours());
+
+/*
+var count;
+for(count=0; count <=5; count++){
+    alert(count);
+}
+*/
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count)
+    count++
+};
+*/
+/*
+var idade = prompt("Qual sua idade?")
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade")
+};
+*/
+
+/*
+var frutas = {{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}}
+console.log(frutas);
+alert(frutas[1].cor);
+*/
+
+/*
+var fruta = {nome:"maçã", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//console.log(lista);
+//console.log(lista.toString());
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+
+//var nome = "Felipe Trevisanutto"
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos";)
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toLoerCase());
+//alert(frase.replace("Japão", "Brail"));
